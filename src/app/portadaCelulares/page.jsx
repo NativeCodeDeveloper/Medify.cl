10ce8ca5fd05b9c133bde986ddfa4e89faac6740
Dan Sepúlveda <dansepulveda@MacBook-Pro-de-Dan.local>
2026-01-30 08:45:41 -0300

    cambio: portada para celulares y algunos textos

--- src/app/portadaCelulares/page.jsx
+++ src/app/portadaCelulares/page.jsx
@@ -13,10 +13,10 @@ export default function PortadaCelulares() {
                 <div className="relative w-full">
                     <div
                         className="h-56 w-full bg-cover bg-center shadow-sm"
-                        style={{ backgroundImage: "url('/pruebitas6.png')" }}
+                        style={{ backgroundImage: "url('/personalsalud.png')" }}
                     />
                     {/* Overlay suave para que se vea más premium sin cambiar paleta */}
-                    <div className="pointer-events-none absolute inset-0 bg-gradient-to-b from-white/0 via-white/0 to-white/70" />
+                    <div className="pointer-events-none absolute inset-0 bg-gradient-to-b from-white/0 via-white/0 to-white/50" />
                 </div>
 
                 <div className="relative mx-auto w-full max-w-md px-6 pb-10 pt-8 text-center">
@@ -24,8 +24,8 @@ export default function PortadaCelulares() {
                     <div className="space-y-4">
 
 
-                        <h1 className={`${spaceGrotesk.className} mx-auto max-w-[22ch] text-balance text-3xl font-bold tracking-tight text-slate-900`}>
-                            Menos desorden.
+                        <h1 className={`${spaceGrotesk.className} mx-auto max-w-[22ch] text-balance text-4xl font-bold tracking-tight text-slate-900`}>
+                            Menos desorden,
                         </h1>
                         <br />
                         <h1 className={`${spaceGrotesk.className} -mt-10 mx-auto max-w-[22ch] text-balance text-3xl font-extrabold tracking-tight text-cyan-600 `}>
@@ -33,20 +33,19 @@ export default function PortadaCelulares() {
                         </h1>
                         <br />
                         <p className="mx-auto text-base font-bold text-pretty  leading-relaxed text-slate-600">
-                            Ordena tu consulta, atrae más pacientes y gestiona tu trabajo.
+                            Gestiona tu consulta de forma profesional y ordenada.
                         </p>
 
-                        <div className="mt-6 flex flex-col items-center gap-3">
-                            <a
-                                href="#"
-                                className="inline-flex w-100 h-10 items-center justify-center rounded-xl border border-cyan-600 bg-white px-6 py-3.5 text-base font-semibold text-cyan-700 shadow-sm transition hover:bg-cyan-50 hover:shadow-md active:scale-[0.995] focus:outline-none focus:ring-2 focus:ring-cyan-500/30"
-                            >
+                        {/* <div className="mt-6 flex flex-col items-center gap-3">
+                            <a href="#"
+                                className="inline-flex w-100 h-10 items-center justify-center rounded-xl border border-cyan-600 bg-white px-6 py-3.5 text-base font-semibold text-cyan-700 shadow-sm transition hover:bg-cyan-50 hover:shadow-md active:scale-[0.995] focus:outline-none focus:ring-2 focus:ring-cyan-500/30">
                                 Comienza
                             </a>
+
                             <p className="text-xs leading-relaxed text-slate-500">
                                 Sin complicaciones • Configuración rápida • Soporte incluido
                             </p>
-                        </div>
+                        </div> */}
 
                         {/* Logo para móviles (visible solo en <md), con texto junto al logo */}
                         <div className="hidden md:hidden" />
